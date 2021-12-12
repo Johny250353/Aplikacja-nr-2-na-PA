5532fb87c0e324e5333f884de3430c2e9bc21a84
Johny250353 <72787337+Johny250353@users.noreply.github.com>
2021-12-12 17:38:27 +0100

    start

---
+++ app/src/main/java/com/example/apkanapa/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.apkanapa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        const val EXTRA_MESSAGE = "com.example.apkanapa.MESSAGE"
+
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText_imie)
+        val message = editText.text.toString()
+        val intent = Intent(this, Hub::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+}
+}
